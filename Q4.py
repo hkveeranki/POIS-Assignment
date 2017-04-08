a55d42ca8c54e9272b5bb225d344917ed1156002
Hemanth Kumar Veeranki <hems.india1997@gmail.com>
2017-04-08 14:59:08 +0530

    Completed Question 4
    Signed-off-by: Hemanth Kumar Veeranki <hemanthveeranki@gmail.com>

--- Q4.py
+++ Q4.py
@@ -1,46 +1,34 @@
 """ Code for Question 4a Commitment API"""
 import hashlib
-import string
-from random import shuffle, randint
+from random import shuffle, randint, sample
 
 import sys
 
+from util_functions import generate_primes, \
+    modinv, ipow, gen_key, gen_str
 
-def commit(msg, key):
+
+def Hash(inp):
+    return hashlib.sha256(inp).hexdigest()
+
+
+def commit(msg):
     """ returns hash of the msg"""
-    return hashlib.sha256(str(msg + key)).hexdigest()
+    key_length = int(pow(2, randint(8, 12)))
+    key = gen_key(key_length)
+    return Hash(str(msg + key)), key
 
 
 def verify(committed, msg, key):
-    return hashlib.sha256(msg + key).hexdigest() == committed
+    return Hash(str(msg + key)) == committed
 
 
-def gen_str(length):
-    """ creates a random string of length len"""
-    ran_str = ""
-    lis = string.letters
-    n = len(lis)
-    for _ in range(length):
-        i = randint(0, n - 1)
-        ran_str += lis[i]
-    return ran_str
-
-
-def gen_key(key_leng):
-    key = ""
-    for j in xrange(key_leng):
-        rb = randint(0, 10 ** 5) % 2
-        key += str(rb)
-    return key
-
-
-def a_4(str_len, key_len):
+def a_4(str_len):
     m = gen_str(str_len)
-    key = gen_key(key_len)
     print "Message", m
-    com = commit(key, m)
-    print "Commit ", com
-    print verify(com, key, m)
+    com, key = commit(m)
+    print "Commit", com
+    print verify(com, m, key)
     dum = list(m)
     while dum == list(m):
         shuffle(dum)
@@ -49,22 +37,65 @@ def a_4(str_len, key_len):
     print verify(com, key, dum)
 
 
-def b_4(str_len, key_len):
+def generate_rsa_keypair():
+    global primes
+    len_p = len(primes)
+    pi, qi = sample(range(1, len_p + 1), 2)
+    p, q = primes[pi], primes[qi]
+    gen_n = p * q
+    phin = (p - 1) * (q - 1)
+    e = 65537
+    d = modinv(e, phin)
+    return gen_n, e, d
+
+
+def commit1(message, rans, sig):
+    com, key = commit(message)
+    return com, key, rans, sig
+
+
+def verify1(com, message):
+    global n, pubk
+    message_v = (com[0] == Hash(str(message + com[1])))
+    person_v = (ipow(com[3], pubk, n) == com[2])
+    if message_v:
+        print "Message match with hash"
+    else:
+        print "Message match with hash"
+    if person_v:
+        print "Same person did it"
+    else:
+        print "different person did it"
+
+
+def b_4(str_len):
     """ We also take the hash of the persons key
     and then make a tuple with this hash and message
     and generate the hash of it 
     """
+    print "Generating RSA Key Pair"
+    global n, pubk
+    n, pubk, prik = generate_rsa_keypair()
+    print "Done..Publishing Info"
+    print "n:", n, "public key:", pubk
+    m = gen_str(str_len)
+    rans = randint(2, 10 ** 7)
+    sig = ipow(rans, prik, n)
+    comTupple = commit1(m, rans, sig)
+    verify1(comTupple, m)
     pass
 
 
 if __name__ == "__main__":
     part = sys.argv[1]
     string_length = randint(32, 10 ** 2)
-    key_length = int(pow(2, randint(6, 10)))
     if part == 'a':
-        a_4(string_length, key_length)
+        a_4(string_length)
     elif part == 'b':
-
-        b_4(string_length, key_length)
+        print("Generating Primes...")
+        global primes
+        primes = generate_primes()
+        print("Done")
+        b_4(string_length)
     else:
         sys.stderr.write('only accepted inputs are a and b')
--- util_functions.py
+++ util_functions.py
@@ -1,4 +1,6 @@
 """ Contains utility functions to help us with computations"""
+import string
+from random import randint
 
 
 def ipow(a, b, N):
@@ -10,3 +12,64 @@ def ipow(a, b, N):
         a = (a * a) % N
         b >>= 1
     return res % N
+
+
+def gcd(u, v):
+    while v:
+        u, v = v, u % v
+    return abs(u)
+
+
+def egcd(a, b):
+    """ Extended GCD Algorithm"""
+    if a == 0:
+        return b, 0, 1
+    else:
+        g, y, x = egcd(b % a, a)
+        return g, x - (b // a) * y, y
+
+
+def modinv(a, m):
+    """ Find a^-1 mod m"""
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % m
+
+
+def generate_primes():
+    """ Generates primes from 10**5 to 10**7 and returns the array """
+    N = 10 ** 7 + 1
+    isP = [True for i in range(N)]
+    isP[0] = isP[1] = False
+    primes = []
+    for i in range(2, N):
+        if isP[i]:
+            for j in range(2 * i, N, i):
+                isP[j] = False
+
+    for j in range(10 ** 5, N):
+        if isP[j]:
+            primes.append(j)
+    return primes
+
+
+def gen_str(length):
+    """ creates a random string of length len"""
+    ran_str = ""
+    lis = string.letters
+    n = len(lis)
+    for _ in range(length):
+        i = randint(0, n - 1)
+        ran_str += lis[i]
+    return ran_str
+
+
+def gen_key(key_leng):
+    """ Generate a random key_leng bit string"""
+    key = ""
+    for j in xrange(key_leng):
+        rb = randint(0, 10 ** 5) % 2
+        key += str(rb)
+    return key
