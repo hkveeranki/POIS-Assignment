1d5482a6ff2e2b5c3fcadcf651750862ddd353db
Hemanth Kumar Veeranki <hems.india1997@gmail.com>
2017-04-10 00:36:41 +0530

    Code formatting
    Signed-off-by: Hemanth Kumar Veeranki <hemanthveeranki@gmail.com>

--- Q1.py
+++ Q1.py
@@ -6,7 +6,6 @@ import time
 
 DATA_FILE = "english.data"
 lis = []
-ind_lis = []
 DEF_LEN = 160
 
 
@@ -18,9 +17,8 @@ def hash_short(message, length=16):
 def make_replication():
     """ Replicate the sample data and"""
     f = open(DATA_FILE, 'r')
-    global lis, ind_lis
+    global lis
     lis = f.readlines()
-    ind_lis = range(len(lis))
     f.close()
 
 
@@ -28,30 +26,37 @@ def hack_hash(length):
     """ Tries to hack the sha1"""
     cnt = 0
     h = {}
-    n = len(lis)
-    taken = {}
-    while True:
-        ind = random.randint(0, n - 1)
-        ind = ind_lis[ind]
-        i = lis[ind]
-        cnt += 1
+    for i in lis:
         val = hash_short(i, length)
         if val not in h:
             h[val] = i
         elif i != h[val]:
-            print "Broke at ", cnt
             print i, hash_short(i, DEF_LEN)
             print h[val], hash_short(h[val], DEF_LEN)
-            break
+            return True
+    return False
 
 
 if __name__ == "__main__":
     length = input("Enter length parameter: ")
-    start_time = time.time()
+    taken = {}
+    start_time = time.time() * 1000
     print("Reading Data")
     make_replication()
-    random.shuffle(ind_lis)
-    print("Done.. took %s seconds to process data" % (time.time() - start_time))
-    start_time = time.time()
-    hack_hash(length)
-    print("took %s seconds to hack" % (time.time() - start_time))
+    print("Done.. took %s milli seconds to process data" % (time.time() - start_time))
+    for i in xrange(4, length + 1, 4):
+        print "\n--- Length ---\n", i
+        start_time = time.time()
+        fl = hack_hash(i)
+        if fl:
+            taken[i] = (time.time() - start_time) * 1000
+            print("took %s milli seconds to hack" % taken[i])
+        else:
+            taken[i] = -1
+            print "Cannot hack with given data for length = ", i
+        print "\n--- Done ---\n"
+    for i in range(4, length + 1, 4):
+        if taken[i] != -1:
+            print "length:", i, "time to hack", taken[i], "milli seconds"
+        else:
+            print "length:", i, "Could Not hack"
--- Q3.py
+++ Q3.py
@@ -3,8 +3,8 @@ import random
 import miller_rabin
 
 x = miller_rabin.MillerRabin(221)
-inp = random.randint(10**6, 10**8)
+inp = random.randint(10 ** 6, 10 ** 8)
 sinp = str(inp)
-print "N =", sinp
+print "N =", sinp, "is_prime", x.is_prime(inp, len(sinp))
 print "Next greatest prime:", x.find_next_greatest_prime(inp, len(sinp))
-print "Liars and witesses", x.find_witness_liars()
+print "Liars and witesses of 221:", x.find_witness_liars()
