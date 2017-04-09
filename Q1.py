909ed91c878f25234e7db01a3c49ef9fbc755856
Hemanth Kumar Veeranki <hems.india1997@gmail.com>
2017-04-09 23:03:15 +0530

    Addeed Working part of Q1
    Signed-off-by: Hemanth Kumar Veeranki <hemanthveeranki@gmail.com>

--- Q1.py
+++ Q1.py
@@ -1,5 +1,13 @@
 """ Deals with breaking the Scheme of Q1"""
 import hashlib
+import random
+
+import time
+
+DATA_FILE = "english.data"
+lis = []
+ind_lis = []
+DEF_LEN = 160
 
 
 def hash_short(message, length=16):
@@ -7,9 +15,43 @@ def hash_short(message, length=16):
     return hashlib.sha1(message).hexdigest()[:length / 4]
 
 
-def hack_hash(message):
-    pass
+def make_replication():
+    """ Replicate the sample data and"""
+    f = open(DATA_FILE, 'r')
+    global lis, ind_lis
+    lis = f.readlines()
+    ind_lis = range(len(lis))
+    f.close()
+
+
+def hack_hash(length):
+    """ Tries to hack the sha1"""
+    cnt = 0
+    h = {}
+    n = len(lis)
+    taken = {}
+    while True:
+        ind = random.randint(0, n - 1)
+        ind = ind_lis[ind]
+        i = lis[ind]
+        cnt += 1
+        val = hash_short(i, length)
+        if val not in h:
+            h[val] = i
+        elif i != h[val]:
+            print "Broke at ", cnt
+            print i, hash_short(i, DEF_LEN)
+            print h[val], hash_short(h[val], DEF_LEN)
+            break
 
 
 if __name__ == "__main__":
-    hack_hash("Hello this is me")
+    length = input("Enter length parameter: ")
+    start_time = time.time()
+    print("Reading Data")
+    make_replication()
+    random.shuffle(ind_lis)
+    print("Done.. took %s seconds to process data" % (time.time() - start_time))
+    start_time = time.time()
+    hack_hash(length)
+    print("took %s seconds to hack" % (time.time() - start_time))
